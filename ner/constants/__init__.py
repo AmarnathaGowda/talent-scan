b8854b0400b38ea75113b282a9a16d171a212723
AmarnathaGowda <amarnath1413@gmail.com>
2024-04-03 13:22:44 +0530

    logger and Exceptions are implemeted

---
+++ ner/constants/__init__.py
@@ -0,0 +1,10 @@
+import os
+from datetime import datetime
+
+TIMESTAMP: str = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+
+ARTIFACTS_DIR = os.path.join("artifacts", TIMESTAMP)
+LOGS_DIR = "logs"
+LOGS_FILE_NAME = "ner.log"
+MODELS_DIR = "models"
+BEST_MODEL_DIR = "best_model"
