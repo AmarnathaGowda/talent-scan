275af362a95016403eddb7368c485f57e38b449f
AmarnathaGowda <amarnath1413@gmail.com>
2024-04-03 13:46:04 +0530

    GCP is implemeted

--- ner/constants/__init__.py
+++ ner/constants/__init__.py
@@ -8,3 +8,10 @@ LOGS_DIR = "logs"
 LOGS_FILE_NAME = "ner.log"
 MODELS_DIR = "models"
 BEST_MODEL_DIR = "best_model"
+
+BUCKET_NAME = "resume-ner-using-bert-24"
+GCP_DATA_FILE_NAME = "archive.zip"
+CSV_DATA_FILE_NAME = "ner.csv"
+GCP_MODEL_NAME = "model.pt"
+
+DATA_INGESTION_ARTIFACTS_DIR = "DataIngestionArtifacts"
